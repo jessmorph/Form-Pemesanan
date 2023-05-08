0ce4b50432b233ea13f596fb45b82f9ec7fbe853
Edwin Pranajaya <47565855+jessmorph@users.noreply.github.com>
2023-05-08 12:34:48 +0700

    data dari laptop

--- form-pemesanan/src/App.js
+++ form-pemesanan/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import React from "react";
 import Home from "./Home";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
