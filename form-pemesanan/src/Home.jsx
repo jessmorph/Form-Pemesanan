7fd48f18320cf2c38c42fbe64a8ef395fea4d81d
Edwin Pranajaya <47565855+jessmorph@users.noreply.github.com>
2023-05-08 12:34:20 +0700

    data dari laptop

--- form-pemesanan/src/Home.jsx
+++ form-pemesanan/src/Home.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { Button, Form } from "react-bootstrap";
-import Formcom from "./components/Form_Com";
-import Logincom from "./components/Login_Com";
+// import { Button, Form } from "react-bootstrap";
+// import Formcom from "./components/Form_Com";
+// import Logincom from "./components/Login_Com";
 import Sidebarcom from "./templates/SideBar_Com";
+import SizeCheck from "./components/tester/SizeCheck";
+import Invoice from "./components/tester/Invoice";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function Home(){
     return(
         <>
             <div>
                 {/* <Logincom></Logincom> */}
-                <Sidebarcom></Sidebarcom>
+                {/* <SizeCheck></SizeCheck> */}
+                <Invoice></Invoice>
                 
             </div>
         </>
